758545656bdf4d2d5b1445ab6bda2b2e38cced9b
AhmedChi <ahmedar61@gmail.com>
2020-08-17 09:20:56 +0100

    Add Completed Project

--- PlanetQuestionaire.java
+++ PlanetQuestionaire.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class PlanetQuestionaire {
     public static void main(String[] args) {
-        System.out.println("Whats the biggest planet in our solor system: \n Jupiter \n Mars \n Earth ");
+
+        String firstAnswer = "Jupiter";
+        String secondAnswer = "Mars";
+        String thridAnswer = "Earth";
+
+        System.out.println("Whats the biggest planet in our solor system: " + firstAnswer+ ", " + secondAnswer + ", " + thridAnswer);        
+
+        String correctAnswer = firstAnswer;
 
         Scanner input = new Scanner(System.in);
 
         String answer = input.next();
 
-        if(answer.equals()
+        if(correctAnswer.equals(answer.toUpperCase())){
+            System.out.println("Correct Answer");
+        } else {
+            System.out.println("Wrong Answer, the Correct answer is " + correctAnswer);
+        }
     }
 }
