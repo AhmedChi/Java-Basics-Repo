6a54dba465c2e90fb3067f8b9b44a62d3e3251fe
AhmedChi <ahmedar61@gmail.com>
2020-08-17 11:19:49 +0100

    Add triangle with constructor

---
+++ Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle{
+    double base;
+    double height;
+    double sideLen1;
+    double sideLen2;
+    double sideLen3;
+
+    //Constructor
+    public Triangle(double base, double height,double sideLen1, double sideLen2, double sideLen3){
+        this.base = base;
+        this.height = height;
+        this.sideLen1 = sideLen1;
+        this.sideLen2 = sideLen2;
+        this.sideLen3 = sideLen3;
+    }
+}
